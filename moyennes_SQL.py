b85a6fa9d9bb71a63d79a5f9af7a207e79f8c937
Alexandre Morlet <morlet@crans.org>
2020-06-27 20:24:21 +0200

    Premier export de .json: notes par matière par thème pour 1 élève

--- moyennes_SQL.py
+++ moyennes_SQL.py
@@ -3,7 +3,7 @@ Script qui récupère la moyenne, pour chaque élève, pour chaque matière (dom
 pour chaque grande compétence (thème), à partir de la base SACoche.
 Tables utilisées: [A COMPLETER]
 """
-
+import json
 ###########################
 #    Connexion à MySQL    #
 ###########################
@@ -50,13 +50,30 @@ for tuple in cursor:
 
 print("###########")
 # Pareil, mais cette fois on voit si on arrive à afficher la matière en plus
-query= ("SELECT rt.theme_id, rt.theme_nom, rd.domaine_nom, s.saisie_note "
+# + variable pour choisir l'élève
+eleve_id = 98
+query= ("SELECT rt.theme_nom, rd.domaine_nom, s.saisie_note "
         "FROM sacoche_saisie AS s, sacoche_user AS u, sacoche_referentiel_item AS ri, "
         "sacoche_referentiel_theme as rt, sacoche_referentiel_domaine as rd "
-        "WHERE s.eleve_id=98 AND ri.item_id = s.item_id "
+        "WHERE s.eleve_id=%d AND ri.item_id = s.item_id "
         "AND rt.theme_id = ri.theme_id AND rd.domaine_id = rt.domaine_id "
         "AND s.saisie_note REGEXP '^[0-9]+$' " # on enlève les AB, NE, NN, ...
-        "LIMIT 30")
+        "LIMIT 30"%eleve_id)
 cursor.execute(query)
-for tuple in cursor:
-    print(tuple)
+#for tuple in cursor:
+#    print(tuple)$
+
+
+## On met les données dans un dictionnaire pour pouvoir les traiter
+resultat_eleve = nested_dict()
+### resultat_eleve[nom_matiere][nom_thème] = [note1, note2, ...]
+for theme_nom, domaine_nom, note in cursor:
+    try:
+        resultat_eleve[domaine_nom][theme_nom]['notes'].append(note)
+    except AttributeError as err:
+        resultat_eleve[domaine_nom][theme_nom]['notes'] = []
+        resultat_eleve[domaine_nom][theme_nom]['notes'].append(note)
+
+print(resultat_eleve)
+with open('temp.json', 'w') as json_file:
+    json.dump(resultat_eleve, json_file, sort_keys=True)
