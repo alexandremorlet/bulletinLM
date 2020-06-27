9c3f0a1d702a9ac6c1ca1cceb091f4f71e432b9a
Alexandre Morlet <morlet@crans.org>
2020-06-27 20:42:59 +0200

    V0.1: export propre de fichier .json avec moyenne /4

--- moyennes_SQL.py
+++ moyennes_SQL.py
@@ -25,39 +25,39 @@ nested_dict = lambda: defaultdict(nested_dict)
 
 
 ### On essaye de récupérer, pour un élève (ex: id_eleve = 324), toutes ses notes regroupées par thème
-query= ("SELECT s.item_id, ri.item_nom, s.saisie_note "
-        "FROM sacoche_saisie AS s, sacoche_user AS u, sacoche_referentiel_item AS ri "
-        "WHERE s.eleve_id=98 AND ri.item_id = s.item_id "
-        "AND s.saisie_note REGEXP '^[0-9]+$' " # on enlève les AB, NE, NN, ...
-        "LIMIT 30")
-cursor.execute(query)
-for tuple in cursor:
-    print(tuple)
+# query= ("SELECT s.item_id, ri.item_nom, s.saisie_note "
+#         "FROM sacoche_saisie AS s, sacoche_user AS u, sacoche_referentiel_item AS ri "
+#         "WHERE s.eleve_id=98 AND ri.item_id = s.item_id "
+#         "AND s.saisie_note REGEXP '^[0-9]+$' " # on enlève les AB, NE, NN, ...
+#         "LIMIT 30")
+# cursor.execute(query)
+# for tuple in cursor:
+#     print(tuple)
 
 print("###########")
 # Pareil, mais cette fois on voit si on arrive à afficher le theme_id et theme_nom qui va bien
-query= ("SELECT rt.theme_id, rt.theme_nom, s.saisie_note "
-        "FROM sacoche_saisie AS s, sacoche_user AS u, sacoche_referentiel_item AS ri, "
-        "sacoche_referentiel_theme as rt "
-        "WHERE s.eleve_id=98 AND ri.item_id = s.item_id "
-        "AND rt.theme_id = ri.theme_id "
-        "AND s.saisie_note REGEXP '^[0-9]+$' " # on enlève les AB, NE, NN, ...
-        "LIMIT 30")
-cursor.execute(query)
-for tuple in cursor:
-    print(tuple)
+# query= ("SELECT rt.theme_id, rt.theme_nom, s.saisie_note "
+#         "FROM sacoche_saisie AS s, sacoche_user AS u, sacoche_referentiel_item AS ri, "
+#         "sacoche_referentiel_theme as rt "
+#         "WHERE s.eleve_id=98 AND ri.item_id = s.item_id "
+#         "AND rt.theme_id = ri.theme_id "
+#         "AND s.saisie_note REGEXP '^[0-9]+$' " # on enlève les AB, NE, NN, ...
+#         "LIMIT 30")
+# cursor.execute(query)
+# for tuple in cursor:
+#     print(tuple)
 
 
 print("###########")
 # Pareil, mais cette fois on voit si on arrive à afficher la matière en plus
 # + variable pour choisir l'élève
-eleve_id = 98
+eleve_id = 404
 query= ("SELECT rt.theme_nom, rd.domaine_nom, s.saisie_note "
         "FROM sacoche_saisie AS s, sacoche_user AS u, sacoche_referentiel_item AS ri, "
         "sacoche_referentiel_theme as rt, sacoche_referentiel_domaine as rd "
         "WHERE s.eleve_id=%d AND ri.item_id = s.item_id "
         "AND rt.theme_id = ri.theme_id AND rd.domaine_id = rt.domaine_id "
-        "AND s.saisie_note REGEXP '^[0-9]+$' " # on enlève les AB, NE, NN, ...
+        #"AND s.saisie_note REGEXP '^[0-9]+$' " # on enlève les AB, NE, NN, ...
         "LIMIT 30"%eleve_id)
 cursor.execute(query)
 #for tuple in cursor:
@@ -75,5 +75,26 @@ for theme_nom, domaine_nom, note in cursor:
         resultat_eleve[domaine_nom][theme_nom]['notes'].append(note)
 
 print(resultat_eleve)
+
+
+
+
+########### Calcul de moyenne
+def calc_moyenne(dico): # on attend de recevoir le dictionnaire resultat_eleve[matière][theme]
+    for matiere in dico:
+        for theme in dico[matiere]:
+            print(matiere, theme)
+            moyenne = 0
+            nb_notes = 0
+            for note in dico[matiere][theme]['notes']:
+                if note.isdigit(): # Ne comptent dans la moyenne que les nombres 1-4
+                    moyenne += int(note)
+                    nb_notes += 1
+            moyenne = moyenne/nb_notes
+            dico[matiere][theme]['moyenne'] = moyenne
+    return dico
+
+resultat_eleve = calc_moyenne(resultat_eleve)
+
 with open('temp.json', 'w') as json_file:
     json.dump(resultat_eleve, json_file, sort_keys=True)
